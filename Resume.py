6f7c98fa5c2637c9ba9f468e2c29d485851c70da
hwhjones <hwh.jones@gmail.com>
2024-09-01 10:26:48 +1000

    Revised with new profile pic, refactored some of the code, now referencing Medium blog

--- Resume.py
+++ Resume.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from pathlib import Path
-from PIL import Image 
+from PIL import Image
 
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "CV.pdf"
+resume_file = current_dir / "assets" / "Harry Jones Technical PM CV.pdf"
 profile_pic = current_dir / "assets" / "profile-pic.jpg"
 
 # --- GENERAL SETTINGS ---
@@ -12,16 +12,17 @@ PAGE_TITLE = "Digital CV | Harry Jones"
 PAGE_ICON = ":random:"
 NAME = "Harry Jones"
 DESCRIPTION = """
-Technical Project Manager skilled in engineering and telecommunications, focused on successful project delivery and effective stakeholder management.
+Technical Project Manager skilled in engineering and telecommunications, with 14 years of experience focused on successful project delivery and effective stakeholder management.
 """
 
-EMAIL = "hwh.jones@email.com"
+EMAIL = "hwh.jones@gmail.com"
 SOCIAL_MEDIA = {
     "LinkedIn": "https://www.linkedin.com/in/harry-jones-97436342/",
     "GitHub": "https://github.com/hwhjones",
 }
 PROJECTS = {
-    "🏆 Hello": "https://youtu.be/Sb0A9i6d320",
+    "🏆 PersonalPage": "https://github.com/hwhjones/PersonalPage",
+    "🏆 JobScrapper": "https://github.com/hwhjones/jobscrape",
 }
 
 st.set_page_config(page_title = PAGE_TITLE, page_icon = PAGE_ICON)
@@ -29,6 +30,7 @@ st.set_page_config(page_title = PAGE_TITLE, page_icon = PAGE_ICON)
 # --- LOAD CSS, PDF & PROFIL PIC ---
 with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+    
 with open(resume_file, "rb") as pdf_file:
     PDFbyte = pdf_file.read()
 profile_pic = Image.open(profile_pic)
@@ -37,7 +39,7 @@ profile_pic = Image.open(profile_pic)
 
 col1, col2 = st.columns(2, gap = "small")
 
-col1.image(profile_pic, width=320)
+col1.image(profile_pic, width=280)
 
 col2.title(NAME)
 col2.write(DESCRIPTION)
@@ -59,10 +61,10 @@ st.write('\n')
 st.subheader("Experience & Qualifications")
 st.write(
     """
-- ✔️ Over 14 years of experience in technical project management and engineering, specializing in signalling and telecommunications
+- ✔️ Over 14 years of experience in technical project management and engineering, specializing in telecommunications and railway signalling
 - ✔️ Proven expertise in software integration, system design, and deployment for complex engineering projects
-- ✔️ Strong skills in data analysis and machine learning with Python to drive informed decision-making
 - ✔️ Effective leader in managing cross-functional teams and ensuring timely project delivery
+- ✔️ Skills in machine learning with Python to drive informed decision-making, with experience in cloud deployment
 """
 )
 
@@ -70,10 +72,9 @@ st.write('\n')
 st.subheader("Project Management Skills")
 st.write(
     """
-- 📈 Project Planning and Execution
+- 📈 Project Management, Planning and Execution
 - 🤝 Stakeholder Management and Client Interface
 - 🔄 System Integration and Third-party Coordination
-- 🚀 Agile and Traditional Project Management Methodologies
 - 💼 Commercial and Technical Supplier Management
 - 🔍 Risk Assessment and Mitigation Strategies
 """
@@ -95,72 +96,72 @@ st.write('\n')
 st.subheader("Professional Experience")
 
 
-st.write("🏗️", "**Technical Project Manager / Senior Project Engineer | Siemens Mobility**")
+st.write("**Technical Project Manager / Senior Project Engineer | Siemens Mobility**")
 st.write("2024 - Present")
 st.write(
     """
-- ► Leading interface management for Sydney Metro West, ensuring seamless client and subcontractor engagement
-- ► Overseeing technical deployment and integration for major infrastructure projects
+- ► Leading interface management for Sydney Metro West Sydney Airport, ensuring seamless client and subcontractor engagement.
+- ► Overseeing technical deployment and integration for major infrastructure projects.
 """
 )
 
 st.write('\n')
-st.write("🌏", "**Technical Project Manager / Senior Project Engineer | Siemens Mobility, New Zealand**")
+st.write("**Technical Project Manager / Senior Project Engineer | Siemens Mobility, New Zealand**")
 st.write("2021 - 2024")
 st.write(
     """
-- ► Managed delivery of signalling systems for Auckland City Rail Link, coordinating with clients and subcontractors
+- ► Managed delivery of signalling systems for Auckland City Rail Link, coordinating with clients and subcontractors.
 """
 )
 
 st.write('\n')
-st.write("🇬🇧", "**Technical Project Manager / Senior Project Engineer | Siemens Mobility, UK**")
+st.write("**Technical Project Manager / Senior Project Engineer | Siemens Mobility, UK**")
 st.write("2019 - 2021")
 st.write(
     """
-- ► Interface Management for Crossrail C620, managing third-party integrations and client communications
+- ► Interface Management for Crossrail C620, managing third-party integrations and client communications.
 """
 )
 
 st.write('\n')
-st.write("📡", "**Technical Project Manager / Senior Project Engineer | Siemens Mobility, UK**")
+st.write("**Technical Project Manager / Senior Project Engineer | Siemens Mobility, UK**")
 st.write("2016 - 2019")
 st.write(
     """
-- ► Managed telecommunications projects, including design, construction, and commissioning for various rail projects
+- ► Managed telecommunications projects, including design, construction, and commissioning for various rail projects.
 """
 )
 
 st.write('\n')
-st.write("🔧", "**Senior Telecommunications Engineer / Tester in Charge | Siemens Mobility, UK**")
+st.write("**Senior Telecommunications Engineer / Tester in Charge | Siemens Mobility, UK**")
 st.write("2015 - 2016")
 st.write(
     """
-- ► Led testing and commissioning for Thameslink London Bridge Area Programme, overseeing network upgrades
+- ► Led testing and commissioning for Thameslink London Bridge Area Programme, overseeing network upgrades. 
 """
 )
 
 st.write('\n')
-st.write("🚄", "**Project Engineer | Ansaldo STS, Australia**")
+st.write("**Project Engineer | Ansaldo STS, Australia**")
 st.write("2014 - 2015")
 st.write(
     """
-- ► Managed engineering and testing for Roy Hill Railway Signalling and Telecommunications Project
+- ► Managed engineering and testing for Roy Hill Railway Signalling and Telecommunications Project.
 """
 )
 
 st.write('\n')
-st.write("🛠️", "**Communications and Control Systems Engineer | Arup, Australia**")
+st.write("**Communications and Control Systems Engineer | Arup, Australia**")
 st.write("2010 - 2012")
 st.write(
     """
-- ► Engaged in project management and systems engineering, including smart grid and SCADA opportunities
+- ► Engaged in project management and systems engineering, including fibre optic and radio design. 
 """
 )
 
 # --- Projects & Accomplishments ---
 st.write('\n')
-st.subheader("Projects & Accomplishments")
+st.subheader("Projects")
 st.write("---")
 for project, link in PROJECTS.items():
     st.write(f"[{project}]({link})")
---
+++ pages/1_Blog.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import feedparser
+from pathlib import Path
+from PIL import Image
+
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+
+# Go up one level to the parent directory (personalpage)
+parent_dir = current_dir.parent
+
+# Now locate the CSS file in the styles folder
+css_file = parent_dir / "styles" / "main.css"
+
+with open(css_file) as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+
+def get_medium_feed(username):
+    feed_url = f"https://medium.com/feed/{username}"
+    return feedparser.parse(feed_url)
+
+d = get_medium_feed("@hwhjones")
+
+# print(d.feed.title)
+
+# print(d.feed.link)
+
+# print(d.feed.description)
+
+# def remove_invisible_chars(text):
+#     return ''.join(ch for ch in text if not unicodedata.category(ch).startswith('C'))
+
+# def strip_html(text):
+#     return re.sub('<[^<]+?>', '', text)
+
+# def clean_text(text):
+#     text = remove_invisible_chars(text)
+#     text = strip_html(text)
+#     return text
+
+for entry in d.entries:
+    st.subheader(entry.title)
+    st.write(f"Published on: {entry.published}")
+    st.markdown(entry.summary, unsafe_allow_html=True)
+    st.markdown(f"[Read more]({entry.link})")
+    st.markdown("---")
